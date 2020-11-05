7a90e34127b344334c0563b104dd1cf4d77174f9
MaxTNielsen <maxtnielsen@gmail.com>
2020-11-05 17:11:56 +0100

    blabla

--- ML_02/Scripts/Max.py
+++ ML_02/Scripts/Max.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
 import numpy as np
+from sklearn import model_selection
 
 from main import *
 
@@ -17,9 +17,38 @@ Test_SC = np.empty(N)
 
 def zero_rule_algorithm_regression(train, test):
 	prediction = np.mean(train, axis=0)
-	predicted = [prediction for i in range(len(test))]
+	predicted = np.asarray([prediction for i in range(len(test))])
 	return predicted
 
 predictions = zero_rule_algorithm_regression(Y_EJ, Test_EJ)
 
-print(predictions)
+print('Predictions'.format(predictions))
+
+def cross_fold_algorithm(X, y):
+        # K-fold crossvalidation
+        K = 10
+        CV = model_selection.KFold(n_splits=K,shuffle=True)
+        
+        Error_train = np.empty((K,1))
+        Error_test = np.empty((K,1))
+        
+        k=0
+        for train_index, test_index in CV.split(X):
+            
+            # extract training and test set for current CV fold
+            X_train = X[train_index,:]
+            y_train = y[train_index]
+            X_test = X[test_index,:]
+            y_test = y[test_index]
+            
+            # Compute squared error
+            Error_train[k] = np.square(y_train-predictions.sum()/len(y_train))
+            Error_test[k] = np.square(y_test-predictions.sum()/len(y_train))
+                                      
+            print('Cross validation fold {0}/{1}'.format(k+1,K))
+            print('Train indices: {0}'.format(train_index))
+            print('Test indices: {0}'.format(test_index))
+            
+            k+=1
+            
+cross_fold_algorithm(X, Y_EJ)
--- ML_02/Scripts/main.py
+++ ML_02/Scripts/main.py
@@ -1,7 +1,6 @@
 # Import packages
 import numpy as np
 import pandas as pd
-import numpy as np
 
 # Load Cardiovascular disease data using pandas
 filename = '../Data/CVD-data.csv'
