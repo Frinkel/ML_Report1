4354e86cdec4836d5a736a13aa06a278462c3ded
MaxTNielsen <maxtnielsen@gmail.com>
2020-11-06 11:09:08 +0100

    bbbb

--- ML_02/Scripts/Max.py
+++ ML_02/Scripts/Max.py
@@ -22,33 +22,38 @@ def zero_rule_algorithm_regression(train, test):
 
 predictions = zero_rule_algorithm_regression(Y_EJ, Test_EJ)
 
-print('Predictions'.format(predictions))
+#print('Predictions', predictions)
 
 def cross_fold_algorithm(X, y):
         # K-fold crossvalidation
         K = 10
+        k = 0
         CV = model_selection.KFold(n_splits=K,shuffle=True)
-        
+       
         Error_train = np.empty((K,1))
         Error_test = np.empty((K,1))
         
-        k=0
+        
         for train_index, test_index in CV.split(X):
             
             # extract training and test set for current CV fold
             X_train = X[train_index,:]
             y_train = y[train_index]
+            p_train = predictions[train_index]
             X_test = X[test_index,:]
             y_test = y[test_index]
+            p_test = predictions[test_index]
             
             # Compute squared error
-            Error_train[k] = np.square(y_train-predictions.sum()/len(y_train))
-            Error_test[k] = np.square(y_test-predictions.sum()/len(y_train))
+            Error_train[k] = np.square(y_train-p_train).sum()/y_train.shape[0]
+            Error_test[k] = np.square(y_test-p_test).sum()/y_test.shape[0]
                                       
-            print('Cross validation fold {0}/{1}'.format(k+1,K))
-            print('Train indices: {0}'.format(train_index))
-            print('Test indices: {0}'.format(test_index))
-            
-            k+=1
+            #print('Cross validation fold {0}/{1}'.format(K,k))
+            #print('Train indices: {0}'.format(train_index))
+            #print('Test indices: {0}'.format(test_index))
+        k += 1
+        
+        print('Error Test', Error_test) 
+        print('Error Train', Error_train)
             
 cross_fold_algorithm(X, Y_EJ)
