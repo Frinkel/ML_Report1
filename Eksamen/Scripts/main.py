106931ec823a4636ca65ac74e8373fb44a0b5b4b
Joel Madsen <joel.madsen10@gmail.com>
2020-12-13 11:15:24 +0100

    Jaccard implemented

--- Eksamen/Scripts/main.py
+++ Eksamen/Scripts/main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+import itertools
 
 
 # This document is created by Joel Madsen, Lasse Damgård and Max Thrane, as a helper document for the exam.
@@ -44,7 +45,38 @@ def averageDistance(init, l1):
     return sum_l1/l1_len
 
 
-
 # Calculates the K nearest neighbor density. K = neighbors, distance = the average distance.
 def knnDensity(distance, K):
     return 1/(1/K*distance)
+
+
+# From https://gist.github.com/ramhiser/c990481c387058f3cce7
+# Calculates the Jaccard similarity between two sets of clustering labels!
+def jaccard(labels1, labels2):
+    """
+    Computes the Jaccard similarity between two sets of clustering labels.
+    The value returned is between 0 and 1, inclusively. A value of 1 indicates
+    perfect agreement between two clustering algorithms, whereas a value of 0
+    indicates no agreement. For details on the Jaccard index, see:
+    http://en.wikipedia.org/wiki/Jaccard_index
+    Example:
+    labels1 = [1, 2, 2, 3]
+    labels2 = [3, 4, 4, 4]
+    print jaccard(labels1, labels2)
+    @param labels1 iterable of cluster labels
+    @param labels2 iterable of cluster labels
+    @return the Jaccard similarity value
+    """
+    n11 = n10 = n01 = 0
+    n = len(labels1)
+    # TODO: Throw exception if len(labels1) != len(labels2)
+    for i, j in itertools.combinations(range(n), 2):
+        comembership1 = labels1[i] == labels1[j]
+        comembership2 = labels2[i] == labels2[j]
+        if comembership1 and comembership2:
+            n11 += 1
+        elif comembership1 and not comembership2:
+            n10 += 1
+        elif not comembership1 and comembership2:
+            n01 += 1
+    return float(n11) / (n11 + n10 + n01)
